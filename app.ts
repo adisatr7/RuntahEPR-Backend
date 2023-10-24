a8b73107e5aade3e35bf016fe485ffc055f80e0b
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-10-24 22:38:40 +0700

    Added CORS

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import { initDB } from "./database/connection.js"
 
 
@@ -8,6 +9,8 @@ const app = express()
 const PORT = 8080
 
 app.use(express.json())
+
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Backend server is running on port ${PORT}!`)
